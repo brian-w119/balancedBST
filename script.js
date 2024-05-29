06a16bab1b6b5e13986662f004ac7f5a4cb3ded0
MyPc <brianw119@outlook.com>
2024-05-29 23:56:41 +0100

    Added function to sort and find middle value of array

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const balancedBST = {
    arr: [],
    sortedArr: [],
    finalArr: [],
+   mid: null,
 
    //clears input fields and storage arrays
    clearAll() {
@@ -16,7 +17,7 @@ const balancedBST = {
       this.arraySize.value = "";
    },
 
-   //creates a single random number
+   //creates a single random number and pshes to an array
    generateRandom() {
       let y = Math.floor(Math.random() * 30);
       this.arr.push(y);
@@ -38,6 +39,7 @@ const balancedBST = {
       }
    },
 
+   //checks for duplicates
    checkDuplicates() {
       let repeated;
       let newArr = new Set(this.arr);
@@ -51,6 +53,69 @@ const balancedBST = {
       return repeated;
    },
 
+   //removes duplicates
+   removeDuplicates() {
+      this.sortedArr = [...new Set(this.arr)];
+      this.sortedArr.sort(function (a, b) {
+         return a - b;
+      });
+      console.log(this.sortedArr);
+      return this.sortedArr;
+   },
+
+   //creates a random number
+   createRandNum() {
+      const num = Math.floor(Math.random() * 30);
+      return num;
+   },
+
+   //adjust length of array if its length is less than original array created
+   adjustLength() {
+      this.display.innerText = "";
+      console.log(
+         `sorted length: ${this.sortedArr.length}, original array: ${this.arr.length}`
+      );
+      while (this.sortedArr.length < this.arr.length) {
+         let number = this.createRandNum();
+         if (this.sortedArr.includes(number)) {
+            number = Math.floor(Math.random() * 30);
+         } else if (!this.sortedArr.includes(number)) {
+            this.sortedArr.push(number);
+         }
+      }
+      console.log(`length adjusted arr: ${this.sortedArr}`);
+      this.sortedArr.sort(function (a, b) {
+         return a - b;
+      });
+      console.log(
+         `final sorted Array: [${this.sortedArr}], length: ${this.sortedArr.length}`
+      );
+      this.display.innerText = this.sortedArr;
+   },
+
+   findRoot() {
+      let mid;
+      let middleValue;
+      if (this.arraySize.value % 2 === 0) {
+         mid = this.sortedArr.length / 2;
+         middleValue = this.sortedArr[mid];
+         console.log(middleValue);
+      } else {
+         mid = this.sortedArr.length / 2;
+         mid = Math.floor(mid);
+         middleValue = this.sortedArr[mid];
+         console.log(middleValue);
+      }
+   },
+
+   Node: class {
+      constructor(left, right, root) {
+         this.leftChild = left;
+         this.rightChild = right;
+         this.root = root;
+      }
+   },
+
    init() {
       this.reset.addEventListener("click", () => this.clearAll());
       this.createList.addEventListener("click", () => {
@@ -58,8 +123,10 @@ const balancedBST = {
             this.clearAll();
          }
          this.createRandom();
-
          this.checkDuplicates();
+         this.removeDuplicates();
+         this.adjustLength();
+         this.findRoot();
       });
    },
 };
